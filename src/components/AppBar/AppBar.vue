96a25d138984d39aa7bf8feecf2eb3e1e3160db4
Tantatorn Suksangwarn <timefear007@gmail.com>
2019-04-07 02:23:37 +0700

    Edit toolbar

--- src/components/AppBar/AppBar.vue
+++ src/components/AppBar/AppBar.vue
@@ -1,38 +1,4 @@
 <template>
-  <!--
-  <v-toolbar app>
-    
-    <a href="/">
-    <img :src="require('@/assets/justchange.png')">
-    </a>
-    <v-spacer></v-spacer>
-    <v-btn
-      flat
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-    >
-      <span class="mr-2">About</span>
-    </v-btn>
-    <v-btn flat to="/map">
-      <span class="mr-2">ChangeNow</span>
-    </v-btn>
-
-    <v-btn
-      flat
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-    >
-      <span class="mr-2">Reward</span>
-    </v-btn>
-    <v-btn
-      flat
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-    >
-      <span class="mr-2">Contact</span>
-    </v-btn>
-    <v-btn color="#013101" dark>Sign up</v-btn>
-  <v-btn outline color="#013101">Log in</v-btn>-->
   <v-toolbar prominent extended>
     <v-toolbar-title>Title</v-toolbar-title>
 
@@ -46,16 +12,20 @@
     <v-btn outline color="#013101">Log in</v-btn>
     <template v-slot:extension>
       <Category></Category>
+      <SearchField></SearchField>
     </template>
   </v-toolbar>
 </template>
 
 <script>
 import Category from "./Category";
+import SearchField from "./SearchField";
+
 export default {
   name: "AppBar",
   components: {
-    Category
+    Category,
+    SearchField
   }
 };
 </script>
---
+++ src/components/AppBar/SearchField.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-text-field label="Solo" placeholder="Search" solo></v-text-field>
+</template>
+    
+<script>
+export default {
+  name: "SearchField",
+};
+</script>
+
+<style scoped>
+</style>
