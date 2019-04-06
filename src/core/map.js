9a0660dbff4214b7517e08f86b9033a8a94d1078
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-04-06 09:52:27 +0700

    Add google map

---
+++ src/core/map.js
@@ -0,0 +1,7 @@
+var GoogleMapsLoader = require('google-maps');
+
+GoogleMapsLoader.KEY = 'AIzaSyCJCDkJmywl1Z23uMFQGo844x0Vrkk57h0';
+GoogleMapsLoader.LIBRARIES = ['drawing'];
+GoogleMapsLoader.VERSION = 'weekly';
+
+export default GoogleMapsLoader;
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,13 +1,27 @@
 <template>
-  <HelloWorld />
+  <div id="map" ref="map" style="height: 100%;"></div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import Map from './../core/map';
 
-  export default {
-    components: {
-      HelloWorld
-    }
+export default {
+  mounted() {
+    var el = this.$refs.map;
+    Map.load(function(google) {
+      var myLatLng = {lat: -25.363, lng: 131.044};
+
+      var map = new google.maps.Map(el, {
+        zoom: 4,
+        center: myLatLng
+      });
+
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+      });
+    });
   }
+}
 </script>
