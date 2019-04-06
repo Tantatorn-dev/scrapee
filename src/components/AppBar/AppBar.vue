4c29a273c39435d39c835fad7bec65a467f0241c
Tantatorn Suksangwarn <timefear007@gmail.com>
2019-04-07 01:25:25 +0700

    Add category dropdown

--- src/components/AppBar/AppBar.vue
+++ src/components/AppBar/AppBar.vue
@@ -1,5 +1,7 @@
 <template>
+  <!--
   <v-toolbar app>
+    
     <a href="/">
     <img :src="require('@/assets/justchange.png')">
     </a>
@@ -30,13 +32,31 @@
       <span class="mr-2">Contact</span>
     </v-btn>
     <v-btn color="#013101" dark>Sign up</v-btn>
+  <v-btn outline color="#013101">Log in</v-btn>-->
+  <v-toolbar prominent extended>
+    <v-toolbar-title>Title</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+      <span class="mr-2">About</span>
+    </v-btn>
+
+    <v-btn color="#013101" dark>Sign up</v-btn>
     <v-btn outline color="#013101">Log in</v-btn>
+    <template v-slot:extension>
+      <Category></Category>
+    </template>
   </v-toolbar>
 </template>
 
 <script>
+import Category from "./Category";
 export default {
-  name: "AppBar"
+  name: "AppBar",
+  components: {
+    Category
+  }
 };
 </script>
 
---
+++ src/components/AppBar/Category.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-xs-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="#FFFFFF"
+          v-on="on"
+        >
+          Category
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-tile
+          v-for="(item, index) in items"
+          :key="index"
+          @click=""
+        >
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Computer and telecommunication' },
+        { title: 'Refrigerator' },
+        { title: 'Toys, leisure' },
+        { title: 'Lighting device' }
+      ]
+    })
+  }
+</script>
