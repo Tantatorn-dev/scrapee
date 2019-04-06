d4d9c158dc155a5ecdbf00277747e73e6432ddfc
Crosszpai <61010556@kmitl.ac.th>
2019-04-06 19:05:16 +0700

    add Login and Register Modal

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app>
     <Achievement></Achievement>
+    <LoginModal/>
   </v-app>
 </template>
 
 <script>
 import Achievement from './views/achievement/Achievement';
+import LoginModal from './components/LoginModal/LoginModal';
 
 export default {
   name: 'App',
   components: {
     Achievement,
+    LoginModal
   },
   data() {
     return {
