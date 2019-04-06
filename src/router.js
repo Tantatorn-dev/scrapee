67a2437769ee04bf22b5e51f364ff1d23f3ee594
Tantatorn Suksangwarn <timefear007@gmail.com>
2019-04-06 11:26:27 +0700

    Bring achievement page back

--- src/router.js
+++ src/router.js
@@ -45,6 +45,12 @@ export default new Router({
       path: "/map",
       name: "map",
       component: () => import(/* webpackChunkName: "map" */ "./views/Map.vue")
+    },
+    {
+      path: "/achievement",
+      name: "achievement",
+      component: () =>
+        import(/*webpackChunkName: "achievement*/ "./views/Achievement.vue")
     }
   ]
 });
