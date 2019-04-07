83148860aab8f6c6dacd75f2744c38793d6bae93
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-04-07 08:20:46 +0700

    Add search params

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,57 +1,75 @@
 <template>
-  <v-container grid-list-md fluid>
-    <v-layout row wrap>
-      <v-flex
-        v-for="n in 40"
-        :key="n"
-        xs2
-        class="border"
-        :class="n % 5 == 1 ? 'offset-xs1' : ''"
-      >
-        <v-card hover>
-          <v-img
-            :src="
-              `https://loremflickr.com/300/300/electronics,component&random=${Math.random()}`
-            "
-            height="200px"
-          >
-            <template v-slot:placeholder>
-              <v-layout fill-height align-center justify-center ma-0>
-                <v-progress-circular
-                  indeterminate
-                  color="grey lighten-1"
-                ></v-progress-circular>
-              </v-layout>
-            </template>
-          </v-img>
-          <v-card-title>
-            <div style="width: 100%;">
-              <div class="text-truncate">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </div>
-              <div class="mt-3">
-                <span primary class="title"
-                  >US ${{ Math.round(Math.random() * 50) + 50 }} -
-                  {{ Math.round(Math.random() * 100) + 70 }}
-                </span>
-                <span class="grey--text">/price</span>
-              </div>
-              <span
-                >{{
-                  new Intl.NumberFormat().format(
-                    Math.round(Math.random() * 2000) + 1
-                  )
-                }}
-                Prices</span
-              >
-            </div>
-          </v-card-title>
+  <div>
+    <v-layout>
+      <v-flex xs12>
+        <v-card dark tile flat color="primary">
+          <v-card-text class="display-1">{{ title }}</v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
-  </v-container>
+    <v-container grid-list-md fluid>
+      <v-layout row wrap>
+        <v-flex v-for="n in 40" :key="n" xs3 class="border">
+          <v-card hover>
+            <v-img
+              :src="
+                `https://loremflickr.com/300/300/${
+                  $route.params.search
+                }&random=${Math.random()}`
+              "
+              height="300px"
+            >
+              <template v-slot:placeholder>
+                <v-layout fill-height align-center justify-center ma-0>
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-1"
+                  ></v-progress-circular>
+                </v-layout>
+              </template>
+            </v-img>
+            <v-card-title>
+              <div style="width: 100%;">
+                <div class="text-truncate">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </div>
+                <div class="mt-3">
+                  <span primary class="title"
+                    >US ${{ Math.round(Math.random() * 50) + 50 }} -
+                    {{ Math.round(Math.random() * 100) + 70 }}
+                  </span>
+                  <span class="grey--text">/price</span>
+                </div>
+                <span
+                  >{{
+                    new Intl.NumberFormat().format(
+                      Math.round(Math.random() * 2000) + 1
+                    )
+                  }}
+                  Prices</span
+                >
+              </div>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    title() {
+      var title = this.$route.params.search;
+      title = title.replace(",", " ");
+      title = title.charAt(0).toUpperCase() + title.slice(1);
+      return title;
+    }
+  }
+};
+</script>
 
 <style scoped>
 .border {
